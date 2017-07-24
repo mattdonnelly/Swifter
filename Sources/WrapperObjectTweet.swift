ba08e7a0b6361b4bdc8184780ff9c2bd7c6bb9aa
zachrwolfe <zachrwolfe@me.com>
2017-07-24 13:52:44 -0500

    Revert "Update iOS demo to use to the wrapper API"
    This reverts commit 90d9591a154ed04d60185a9c60bc1b0edbf50349.

--- Sources/WrapperObjectTweet.swift
+++ Sources/WrapperObjectTweet.swift
@@ -13,23 +13,18 @@ extension Swifter {
         public let text: String
         public let user: User
         public let entities: Entities
-        public let id: UInt64
-        public let idStr: String
         public let retweetedStatus: Tweet?
         
-        init(text: String, user: User, entities: Entities, id: UInt64, idStr: String, retweetedStatus: Tweet? = nil) {
+        init(text: String, user: User, entities: Entities, retweetedStatus: Tweet? = nil) {
             self.text = text
             self.user = user
             self.entities = entities
-            self.id = id
-            self.idStr = idStr
             self.retweetedStatus = retweetedStatus
         }
         
         enum CodingKeys: String, CodingKey {
-            case text, user, entities, id
+            case text, user, entities
             case retweetedStatus = "retweeted_status"
-            case idStr = "id_str"
         }
     }
 }
