88068d921c37596251f88327597a9f37f9945f42
zachrwolfe <zachrwolfe@me.com>
2017-07-25 12:27:21 -0500

    Genericize and specialize SwifterSearch.swift

--- Sources/SwifterSearch.swift
+++ Sources/SwifterSearch.swift
@@ -37,7 +37,7 @@ public extension Swifter {
      In API v1.1, the response format of the Search API has been improved to return Tweet objects more similar to the objects you’ll find across the REST API and platform. However, perspectival attributes (fields that pertain to the perspective of the authenticating user) are not currently supported on this endpoint.
 
      */
-    public func searchTweet(using query: String, geocode: String? = nil, lang: String? = nil, locale: String? = nil, resultType: String? = nil, count: Int? = nil, until: String? = nil, sinceID: String? = nil, maxID: String? = nil, includeEntities: Bool? = nil, callback: String? = nil, success: ((JSON, _ searchMetadata: JSON) -> Void)? = nil, failure: @escaping FailureHandler) {
+    public func searchTweet<T: Decodable>(using query: String, geocode: String? = nil, lang: String? = nil, locale: String? = nil, resultType: String? = nil, count: Int? = nil, until: String? = nil, sinceID: String? = nil, maxID: String? = nil, includeEntities: Bool? = nil, callback: String? = nil, successType: T.Type, success: SuccessHandler<T>? = nil, failure: @escaping FailureHandler) {
         let path = "search/tweets.json"
 
         var parameters = Dictionary<String, Any>()
@@ -53,9 +53,12 @@ public extension Swifter {
         parameters["include_entities"] ??= includeEntities
         parameters["callback"] ??= callback
 
-        self.getJSON(path: path, baseURL: .api, parameters: parameters, success: { json, _ in
-            success?(json["statuses"], json["search_metadata"])
-            }, failure: failure)
+        self.getJSON(path: path, baseURL: .api, parameters: parameters, successType: successType, success: { json, _ in success?(json) }, failure: failure)
     }
     
+    public func searchTweet(using query: String, geocode: String? = nil, lang: String? = nil, locale: String? = nil, resultType: String? = nil, count: Int? = nil, until: String? = nil, sinceID: String? = nil, maxID: String? = nil, includeEntities: Bool? = nil, callback: String? = nil, success: ((JSON, _ searchMetadata: JSON) -> Void)? = nil, failure: @escaping FailureHandler) {
+        self.searchTweet(using: query, geocode: geocode, lang: lang, locale: locale, resultType: resultType, count: count, until: until, sinceID: sinceID, maxID: maxID, includeEntities: includeEntities, callback: callback, successType: JSON.self, success: { json in
+            success?(json["statuses"], json["search_metadata"])
+        }, failure: failure)
+    }
 }
