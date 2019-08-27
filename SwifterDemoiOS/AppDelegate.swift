ac923f8464a4a6a4b6c29237f66907427fd49d93
dazy <csl.nodajima@gmail.com>
2019-08-28 00:49:55 +0900

    fix missing argument

--- SwifterDemoiOS/AppDelegate.swift
+++ SwifterDemoiOS/AppDelegate.swift
@@ -32,7 +32,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     var window: UIWindow?
 
     func application(_ app: UIApplication, open url: URL, options: [UIApplication.OpenURLOptionsKey : Any] = [:]) -> Bool {
-        Swifter.handleOpenURL(url)
+        Swifter.handleOpenURL(url, callbackURL: url)
         return true
     }
     
